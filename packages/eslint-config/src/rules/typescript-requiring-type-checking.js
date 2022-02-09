f4388109e6edf609ce6d0eaaef7d6c262d702488
dangreen <danon0404@gmail.com>
2022-02-10 01:51:27 +0700

    feat(eslint-config): disable @typescript-eslint/prefer-nullish-coalescing

--- packages/eslint-config/src/rules/typescript-requiring-type-checking.js
+++ packages/eslint-config/src/rules/typescript-requiring-type-checking.js
@@ -22,7 +22,7 @@ module.exports = {
     '@typescript-eslint/no-unnecessary-qualifier': 'error',
     '@typescript-eslint/no-unnecessary-type-arguments': 'error',
     '@typescript-eslint/prefer-includes': 'error',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
     '@typescript-eslint/prefer-readonly': 'error',
     '@typescript-eslint/prefer-readonly-parameter-types': 'off',
     '@typescript-eslint/prefer-reduce-type-parameter': 'error',
