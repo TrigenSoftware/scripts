000251fb56bc1abd977b60f09f8b4257c5a25169
dangreen <danon0404@gmail.com>
2022-05-02 22:32:02 +0700

    feat(lint-package-json): ignore option

--- packages/lint-package-json/src/index.js
+++ packages/lint-package-json/src/index.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
+import {
+  readOptions,
+  alias,
+  option
+} from 'argue-cli'
 import NpmPackageJsonLint from 'npm-package-json-lint/src/NpmPackageJsonLint.js'
 import Reporter from 'npm-package-json-lint/src/Reporter.js'
 import configs from './configs.cjs'
 
-const monorepo = process.argv.includes('--monorepo')
+const {
+  monorepo,
+  ignore
+} = readOptions(
+  option(alias('monorepo', 'm'), Boolean),
+  option(alias('ignore', 'i'), Array),
+)
 const config = {
   extends: monorepo
     ? [configs.base, configs.monorepo]
@@ -12,7 +23,7 @@ const config = {
 const npmPackageJsonLint = new NpmPackageJsonLint({
   cwd: process.cwd(),
   config,
-  patterns: ['.']
+  patterns: ['.'].concat(ignore.map(_ => `!${_}`))
 })
 const linterOutput = npmPackageJsonLint.lint()
 
