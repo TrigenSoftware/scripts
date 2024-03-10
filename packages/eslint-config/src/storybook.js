fce9c1014175cdffec3ad698abbdae712aa6f6bd
dangreen <danon0404@gmail.com>
2024-03-10 22:30:49 +0400

    fix(eslint-config): fix rulres for storybook

--- packages/eslint-config/src/storybook.js
+++ packages/eslint-config/src/storybook.js
@@ -14,14 +14,15 @@ const extensions = [
 module.exports = {
   overrides: [
     {
-      files: makePatterns(postfixes, extensions),
+      files: makePatterns(postfixes, extensions).concat('.storybook/**/*'),
       rules: {
         'max-classes-per-file': 'off',
         'no-magic-numbers': 'off',
         '@typescript-eslint/no-magic-numbers': 'off',
         'max-nested-callbacks': 'off',
         'react/no-array-index-key': 'off',
-        'react/jsx-no-bind': 'off'
+        'react/jsx-no-bind': 'off',
+        'import/no-default-export': 'off'
       }
     }
   ]
