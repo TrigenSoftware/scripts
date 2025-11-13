4ec0a0b5aa432a15946b9604b5d4f4dbda3f77a4
dangreen <danon0404@gmail.com>
2025-11-14 02:33:57 +0400

    fix(eslint-config): fix typescript rules for js files

--- packages/eslint-config/src/typescript-type-checked.js
+++ packages/eslint-config/src/typescript-type-checked.js
@@ -4,5 +4,12 @@
 
 import typescriptEslint from 'typescript-eslint'
 import typescriptTypeCheckedConfig from './subconfigs/typescript-type-checked.js'
+import { tsFiles } from './subconfigs/files.js'
 
-export default [...typescriptEslint.configs.recommendedTypeChecked, ...typescriptTypeCheckedConfig]
+export default [
+  ...typescriptEslint.configs.recommendedTypeChecked.map(config => ({
+    ...config,
+    files: tsFiles
+  })),
+  ...typescriptTypeCheckedConfig
+]
--- packages/eslint-config/src/typescript.js
+++ packages/eslint-config/src/typescript.js
@@ -4,5 +4,12 @@
 
 import typescriptEslint from 'typescript-eslint'
 import typescriptConfig from './subconfigs/typescript.js'
+import { tsFiles } from './subconfigs/files.js'
 
-export default [...typescriptEslint.configs.recommended, ...typescriptConfig]
+export default [
+  ...typescriptEslint.configs.recommended.map(config => ({
+    ...config,
+    files: tsFiles
+  })),
+  ...typescriptConfig
+]
